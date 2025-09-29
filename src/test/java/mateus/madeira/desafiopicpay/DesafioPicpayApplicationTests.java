9c17672d44aee04eae1e0b8b5c84b9dc802411b2
Mateus Madeira <mafemad2005@gmail.com>
2025-09-29 14:14:50 -0300

    feat: setup projeto e dependências

---
+++ src/test/java/mateus/madeira/desafiopicpay/DesafioPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package mateus.madeira.desafiopicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicpayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
